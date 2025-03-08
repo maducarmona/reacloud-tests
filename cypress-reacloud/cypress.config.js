c2c355a8e0d95f33229134fe0e6476feaa9fdfd6
Maria Eduarda Carmona <eduardacarmona@gmail.com>
2025-03-08 00:11:37 -0300

    Change viewport size

--- cypress-reacloud/cypress.config.js
+++ cypress-reacloud/cypress.config.js
@@ -3,5 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173', 
+    viewportWidth: 1920,
+    viewportHeight: 1080,
   },
 });
